5a1458e043b687d503e89b2c36dbd1f40269044e
kongshaohua <2454880013@qq.com>
2019-08-07 19:06:39 +0800

    http Post and route

--- chapter4-Route/server.js
+++ chapter4-Route/server.js
@@ -6,8 +6,13 @@ function start(route){
         var pathname=url.parse(req.url).pathname;
         console.log("Request for " + pathname + " received.");
         route(pathname);
-        res.writeHead(200,{'Content-Type':'text/plain'});
-        res.end('hello world');
+        res.writeHead(200,{'Content-Type':'text/plain;charset=utf-8'});
+        //获取Get内容-解析URL参数
+        var params=url.parse(req.url,true).query;
+        res.write("网站名："+params.name);
+        res.write("\n");
+        res.write("网站url："+params.url);
+        res.end();
     }
     http.createServer(onRequest).listen(8090,'127.0.0.1');
     console.log('Http server has started.');
